827204a05deef145d37bf64d03261c5eb51f3a73
ashishthakor <ashuthakor927@gmail.com>
2022-02-04 18:30:07 +0530

    Making Chat App fronend more responsive to user(disabling buttons and focusing on input field)

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,23 @@
 const socket = io();
 
+// elemnts
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#sendLocation');
 socket.on('message', (msg) => {
   console.log(`Welcome!! ${msg}`);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  $messageFormButton.setAttribute('disabled', 'disabled');
 
   const message = e.target.elements.message.value;
   socket.emit('sendMessage', message, (error) => {
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
     if (error) {
       return console.log(error);
     }
@@ -17,10 +26,11 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
   });
 });
 
-document.querySelector('#sendLocation').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser');
   }
+  $sendLocationButton.setAttribute('disabled', 'disabled');
   navigator.geolocation.getCurrentPosition((position) => {
     // console.log(position);
     socket.emit(
@@ -30,6 +40,7 @@ document.querySelector('#sendLocation').addEventListener('click', () => {
         longitude: position.coords.longitude,
       },
       () => {
+        $sendLocationButton.removeAttribute('disabled');
         console.log('location shared');
       }
     );

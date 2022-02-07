e8f6c7b039790c0e905b820ba446af3d1eeee332
ashishthakor <ashuthakor927@gmail.com>
2022-02-07 17:37:16 +0530

    Update Admin Message Final Commit

--- src/index.js
+++ src/index.js
@@ -42,7 +42,10 @@ io.on('connection', (socket) => {
     socket.emit('message', generateMessage('Admin', 'Welcome!'));
     socket.broadcast
       .to(user.room)
-      .emit('message', generateMessage(`Admin. ${user.username} has joined`));
+      .emit(
+        'message',
+        generateMessage('Admin', ` ${user.username} has joined`)
+      );
 
     io.to(user.room).emit('roomData', {
       room: user.room,
